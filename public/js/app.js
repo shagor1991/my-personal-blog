05985b7ad9f9a616ac5e378991f9a2525b3c5f98
Shagor Sarder <cadetshagor@gmail.com>
2020-08-09 10:31:39 +0600

    HomeComponent adding

--- public/js/app.js
+++ public/js/app.js
@@ -50345,8 +50345,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! K:\PHP7.4\htdocs\laravel\shagor-personal-blog\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! K:\PHP7.4\htdocs\laravel\shagor-personal-blog\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\xampp\htdocs\laravel\vue\shagor-personal-blog\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\xampp\htdocs\laravel\vue\shagor-personal-blog\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
