ee5adef55f36845fa8d4391c34beabe640f9e4c8
Shagor Sarder <cadetshagor@gmail.com>
2020-08-13 18:40:32 +0600

    Tag create, and list functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,47 @@ window.Vue = require('vue');
 
 import VueRouter from 'vue-router'
 import {routes} from './routes'
+import { Form, HasError, AlertError } from 'vform'
+import VueProgressBar from 'vue-progressbar'
+import Snotify, { SnotifyPosition } from 'vue-snotify'
+
+window.Form= Form
 Vue.use(VueRouter)
 const router = new VueRouter({
     routes // short for `routes: routes`
 })
 
+const VueProgressBarOptions = {
+    color: '#bffaf3',
+    failedColor: '#874b4b',
+    thickness: '5px',
+    transition: {
+      speed: '0.2s',
+      opacity: '0.6s',
+      termination: 300
+    },
+    autoRevert: true,
+    location: 'left',
+    inverse: false
+}
+  
+Vue.use(VueProgressBar, VueProgressBarOptions)
+
+const SnotifyOptions = {
+    toast: {
+      position: SnotifyPosition.rightTop
+    }
+  }
+  
+Vue.use(Snotify, SnotifyOptions)
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('admin-home', require('./components/AdminHome.vue').default);
 Vue.component('login-component', require('./components/LoginComponent.vue').default);
 
+
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 const app = new Vue({
     router,
     el: '#app',
--- resources/js/components/TagComponent.vue
+++ resources/js/components/TagComponent.vue
@@ -2,19 +2,19 @@
     <div>
         <!-- Content Header (Page header) -->
         <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>Form Validation</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Validation</li>
-                </ol>
-            </div>
-            </div>
-        </div><!-- /.container-fluid -->
+            <div class="container-fluid">
+                <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Form Validation</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Validation</li>
+                    </ol>
+                </div>
+                </div>
+            </div><!-- /.container-fluid -->
         </section>
 
         <!-- Main content -->
@@ -24,15 +24,12 @@
                     <div class="col-md-12">
                         <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Bordered Table</h3>
+                            <h3 class="card-title">All Tags</h3>
                             <div class="card-tools">
-                            <div class="input-group input-group-sm" style="width: 150px;">
-                                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                                <div class="input-group-append">
-                                <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                                </div>
-                            </div>
+                                <button type="button" class="btn btn-info" @click="create">
+                                    Add New
+                                    <i class="fas fa-plus"></i>
+                                </button>
                             </div>
                         </div>
                         <!-- /.card-header -->
@@ -40,53 +37,34 @@
                             <table class="table table-bordered table-hover">
                             <thead>                  
                                 <tr>
-                                <th style="width: 10px">#</th>
+                                <th>#</th>
                                 <th>Tag Name</th>
-                                <th>Progress</th>
-                                <th style="width: 40px">Label</th>
+                                <th>Slug</th>
+                                <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                <td>1.</td>
-                                <td>Update software</td>
-                                <td>
-                                    <div class="progress progress-xs">
-                                    <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                    </div>
-                                </td>
-                                <td><span class="badge bg-danger">55%</span></td>
+                                <tr v-for="(customer, index) in customers" :key="customer.id">
+                                    <td>{{index+1}}</td>
+                                    <td>{{customer.name}}</td>
+                                    <td>
+                                        {{customer.slug}}
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-primary">
+                                            <i class="fas fa-eye"></i>
+                                        </button>
+
+                                        <button class="btn btn-primary">
+                                            <i class="fas fa-edit"></i>
+                                        </button>
+
+                                        <button class="btn btn-danger">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </td>
                                 </tr>
-                                <tr>
-                                <td>2.</td>
-                                <td>Clean database</td>
-                                <td>
-                                    <div class="progress progress-xs">
-                                    <div class="progress-bar bg-warning" style="width: 70%"></div>
-                                    </div>
-                                </td>
-                                <td><span class="badge bg-warning">70%</span></td>
-                                </tr>
-                                <tr>
-                                <td>3.</td>
-                                <td>Cron job running</td>
-                                <td>
-                                    <div class="progress progress-xs progress-striped active">
-                                    <div class="progress-bar bg-primary" style="width: 30%"></div>
-                                    </div>
-                                </td>
-                                <td><span class="badge bg-primary">30%</span></td>
-                                </tr>
-                                <tr>
-                                <td>4.</td>
-                                <td>Fix and squish bugs</td>
-                                <td>
-                                    <div class="progress progress-xs progress-striped active">
-                                    <div class="progress-bar bg-success" style="width: 90%"></div>
-                                    </div>
-                                </td>
-                                <td><span class="badge bg-success">90%</span></td>
-                                </tr>
+                                                              
                             </tbody>
                             </table>
                         </div>
@@ -111,13 +89,76 @@
             </div><!-- /.container-fluid -->
         </section>
         <!-- /.content -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="TagModalLong" tabindex="-1" role="dialog" aria-labelledby="TagModalLongTitle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="TagModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <form role="form" @submit.prevent="store()">
+                    <div class="modal-body">                
+                        <div class="card-body">
+                        <div class="form-group">
+                            <label for="tag_name">Tag Name</label>
+                            <input type="text" v-model="form.name" class="form-control" id="tag_name" placeholder="Tag Name">
+                        </div>                       
+                        </div>
+                        <!-- /.card-body -->                
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return {
+                customers: [],
+                form: new Form({
+                    name: ''
+                })
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.getData()
+        },
+        methods:{
+            getData(){
+                axios.get('/api/tag')
+                .then((response)=>{
+                    this.customers= response.data.tags.data
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+            },
+            create(){
+                $('#TagModalLong').modal('show')
+            },
+            store(){
+                this.form.post('/api/tag')
+                .then(response=>{
+                    this.getData()
+                    $('#TagModalLong').modal('hide')
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+            }
         }
     }
 </script>
